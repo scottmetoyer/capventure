4cf889305a96645688f9393f4ef7430146db60f2
Scott Metoyer <scott.metoyer@gmail.com>
2025-07-30 12:54:19 -0700

    Setup iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.scottmetoyer.capventure',
+  appName: 'CapVenture',
+  webDir: 'dist'
+};
+
+export default config;
